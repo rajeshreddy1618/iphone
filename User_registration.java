0099bb1a892e67d534d8cfe5c31fc370867db8d8
rajeshreddy1618 <33713111+rajeshreddy1618@users.noreply.github.com>
2018-09-25 13:50:23 +0530

    User_registration.ava
    add regeistration file

---
+++ User_registration.java
@@ -0,0 +1,39 @@
+package org.webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class User_registration {
+
+	public static void main(String[] args) throws InterruptedException 
+	{
+		
+		//launch browser window and load application
+		WebDriver driver=new FirefoxDriver();
+		driver.get("http://hotmail.com");
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.id("signup")).click();
+		
+		driver.findElement(By.id("FirstName")).clear();
+		driver.findElement(By.id("FirstName")).sendKeys("newuser");
+		
+		driver.findElement(By.id("LastName")).clear();
+		driver.findElement(By.id("LastName")).sendKeys("webdriver");
+		
+		driver.findElement(By.id("MemberName")).clear();
+		driver.findElement(By.id("MemberName")).sendKeys("newuserwebdriver123456");
+		
+		driver.findElement(By.id("MemberName")).click();
+		Thread.sleep(4000);
+		
+		
+		
+		driver.findElement(By.id("Password")).clear();
+		driver.findElement(By.id("Password")).sendKeys("hello12345");
+
+	}
+
+}
